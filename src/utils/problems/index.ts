a6ef62bfd9157daa4f4ab4cdd8989cdc765fb913
vin2004 <am2sv2004@gmail.com>
2024-03-14 14:32:18 +0530

    Changes problem

--- src/utils/problems/index.ts
+++ src/utils/problems/index.ts
@@ -1,7 +1,5 @@
 import { Problem } from "../types/problem";
-import { jumpGame } from "./jump-game";
-import { reverseLinkedList } from "./reverse-linked-list";
-import { search2DMatrix } from "./search-a-2d-matrix";
+import { palindromeNumber } from "./palindrome-number";
 import { twoSum } from "./two-sum";
 import { validParentheses } from "./valid-parentheses";
 
@@ -11,8 +9,6 @@ interface ProblemMap {
 
 export const problems: ProblemMap = {
 	"two-sum": twoSum,
-	"reverse-linked-list": reverseLinkedList,
-	"jump-game": jumpGame,
-	"search-a-2d-matrix": search2DMatrix,
 	"valid-parentheses": validParentheses,
+	"palindrome-number": palindromeNumber,	
 };
